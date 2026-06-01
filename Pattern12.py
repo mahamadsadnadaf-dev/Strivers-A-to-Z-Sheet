c2af9777fad9888ee08eb038c03b169f9ff1a2a6
saad <saad.nadaf.dev@gmail.com>
2026-06-01 18:04:52 +0530

    Pattern 12 solved

---
+++ Pattern12.py
@@ -0,0 +1,7 @@
+def pattern(n):
+    for i in range(1,n+1):
+        left = "".join(str(j) for j in range(1,i+1))
+        spaces = " "*(2*(n-i))
+        right = "".join(str(j) for j in range(i,0,-1))
+        print(left+spaces+right)
+pattern(4)
